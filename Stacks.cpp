b77710bc111930aa193edeaba4d9f09d0335f1ed
Andy <andycocha6@gmail.com>
2023-12-13 18:09:41 -0500

    STACKS NOTES

--- Stacks.cpp
+++ Stacks.cpp
@@ -31,8 +31,73 @@ VIZUALIZATION:
         [10] last in so first out also 10 would be top element in stack 
         [5] 
 
+ITERATE:
+    To itertate through a stack its different than a list or vector. A stack does not provide direct access to its elements in this way.
+    (for example in array you can do array[i] in a for loop to iterate) in a stack you can't do that
+
+    *Need to use a while loop to iterate a stack
+
+
 EXTRA NOTES:
     A stack is basically just a vector. meaning we dont need to know the size of the stack. 
     It will just dynamically set new space in the stack when needed
 
 */
+
+
+
+#include <iostream>
+#include <stack>
+//example of a simple stack
+
+
+int main(){
+//created stack 
+std::stack<int> example_stack; 
+
+//add elements to stack
+                        //verticle
+example_stack.push(1);  //  5 (top of stack) since 5 was added last
+example_stack.push(2);  //  4
+example_stack.push(3);  //  3
+example_stack.push(4);  //  2
+example_stack.push(5);  //  1   
+//Horizontal: [1,2,3,4,5] top of stack = 5 since added last/recently added
+
+
+//*TRAVERSE THROUGH A STACK
+//print elements of stack before poping an element 
+
+std::cout << "stack before popping " << std::endl;
+while(example_stack.size() != 0){
+    //will repeat until stack is empty
+    std::cout << example_stack.top() << " ";; //print top element of stack
+    example_stack.pop(); //remove the top element of stack to get to the next element/top element of stack
+}
+// OUTPUT: [5,4,3,2,1]
+
+std::cout << std::endl;
+
+
+//add elements to stack again since we popped them for printing
+example_stack.push(1);  
+example_stack.push(2);  
+example_stack.push(3);  
+example_stack.push(4);  
+example_stack.push(5);  
+
+
+example_stack.pop(); //remove top/recently most added element of stack.  removed: 5
+
+
+std::cout << "stack after popping: " << std::endl;
+//print stack after poping element
+while(!example_stack.empty()){
+    std::cout << example_stack.top() << " ";;
+    example_stack.pop();
+}
+
+//OUTPUT: [4,3,2,1]
+
+}
+
